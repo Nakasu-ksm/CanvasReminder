5625132c3824fdd4a39c0d331fab8e5cebb3706c
Doraemon <42112059+dora-world@users.noreply.github.com>
2021-11-06 21:37:41 +0800

    v1.5

--- reminder.py
+++ reminder.py
@@ -1,18 +1,25 @@
-# -*- coding: utf-8 -*-
+# coding=utf-8
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
 import time
+from pygame.mixer import music
+import pygame
 import schedule
 from PyQt5.QtCore import Qt
 import configparser
 import requests
 import webbrowser
+pygame.init()
 config = configparser.ConfigParser() # 类实例化
-config.read("config.ini")
+config.read("config.ini",encoding='utf-8')
 assignment = {}
 web = config['DEFAULT']['website']
 remind = config['TIME']['remindtime']
 token = config['DEFAULT']['APITOKEN']
+ismusic = config['CONFIG']['RemindMusic']
+musicname = config['CONFIG']['RemindMusicName']
+Music = music.load(musicname)
+
 headers = {
     'Authorization':"Bearer {}".format(token)
 }
@@ -22,7 +29,6 @@ q.setWindowFlag(Qt.WindowStaysOnTopHint)
 import json
 import datetime
 def convert_time_to_str(time):
-    #时间数字转化成字符串，不够10的前面补个0
     if (time < 10):
         time = '0' + str(time)
     else:
@@ -50,7 +56,7 @@ def sec_to_data(y):
         return d + "天" + h + "小时" + m + "分钟" + s + "秒"
 
 
-def show_homework(homework,timeStamp, calender_activity,endtime,today):
+def show_homework(homework,timeStamp, calender_activity,endtime,today,LastLoop):
     if calender_activity == 0:
         if (homework['course_id'] == 226 or homework['course_id'] == 227) :
             dateArray = datetime.datetime.fromtimestamp(endtime)
@@ -63,14 +69,17 @@ def show_homework(homework,timeStamp, calender_activity,endtime,today):
     #print(homework['plannable']['title'])
     endint = endtime - int(time.mktime(time.localtime()))
     enddate = sec_to_data(endint)
+    if ismusic == "True":
+        if not music.get_busy():
+            music.play()
     if calender_activity == 1:
         reply = QMessageBox.warning(q, '日历项目', '你有日历项目\n名称:' + homework["plannable"][
             'title'] + '\n点击yes进入详情页面！' + "\n结束时间:" + timeStamp+"\n剩余时间:"+enddate, QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
             webbrowser.open(homework['html_url'], new=0, autoraise=True)
             # 打开页面
-        else:
-            return
+
+
     else:
         if homework['submissions']['submitted'] == True:
             return
@@ -83,8 +92,14 @@ def show_homework(homework,timeStamp, calender_activity,endtime,today):
             # 打开页面
         else:
             QMessageBox.information(q, "Canvas作业助手", "请尽快提交作业吧" + "\n上交时间:" + timeStamp, QMessageBox.Yes)
+    if LastLoop:
+        if ismusic == "True":
+            # 每个窗口播放歌曲
+            if music.get_busy():
+                music.stop()
 
     #sys.exit(0)
+
 
 def run():
     homeworktime = time.strftime("%Y-%m-%d", time.localtime())
@@ -94,43 +109,58 @@ def run():
     # cur.execute()
     try:
         getjson = requests.get('{}/api/v1/planner/items?start_date='.format(web)+homeworktime, headers=headers)
-        js = json.loads(getjson.text)
-        for jsloop in js:
-            calender_activity = 0
-            if jsloop['plannable_type'] == "calendar_event" or jsloop['plannable_type'] == "planner_note":
-                # due = jsloop['plannable']['end_at']
-                due = jsloop['plannable_date']
 
-                calender_activity = 1
-            else:
-                try:
-                    due = jsloop['plannable']['due_at']
-                    if jsloop['plannable']['due_at'] == None:
-                        due = jsloop['plannable_date']
-
-
-                except:
-
-                    continue
-
-            if due:
-                timeArray = time.strptime(due, "%Y-%m-%dT%H:%M:%SZ")
-                endtime = int(time.mktime(timeArray)) + 28800  # 东八
-                dateArray = datetime.datetime.fromtimestamp(endtime)
-                timeStamp = dateArray.strftime("%Y年%m月%d日-%H点%M分%S秒")
-                if not assignment.get(jsloop['plannable']['id']):
-                    assignment[jsloop['plannable']['id']] = int(time.mktime(time.localtime()))
-                    show_homework(jsloop, timeStamp, calender_activity, endtime, homeworktime)
-                util_time = int(time.mktime(time.localtime())) - int(assignment[jsloop['plannable']['id']])
-                if util_time >= int(remind):
-                    # 只显示今日的
-
-                    assignment[jsloop['plannable']['id']] = int(time.mktime(time.localtime()))
-                    show_homework(jsloop, timeStamp, calender_activity, endtime, homeworktime)
-                else:
-                    continue
     except:
         return
+
+    js = json.loads(getjson.text)
+    last = len(js)
+    count = 0
+    LastLoop = False
+    for jsloop in js:
+        if last == count+1:
+            LastLoop = True
+        count = count + 1
+        calender_activity = 0
+        if jsloop['plannable_type'] == "calendar_event" or jsloop['plannable_type'] == "planner_note":
+            # due = jsloop['plannable']['end_at']
+            due = jsloop['plannable_date']
+
+            calender_activity = 1
+        else:
+            try:
+                due = jsloop['plannable']['due_at']
+                if jsloop['plannable']['due_at'] == None:
+                    due = jsloop['plannable_date']
+
+
+            except:
+
+                continue
+
+
+
+
+        if due:
+            timeArray = time.strptime(due, "%Y-%m-%dT%H:%M:%SZ")
+            endtime = int(time.mktime(timeArray)) + 28800  # 东八
+            dateArray = datetime.datetime.fromtimestamp(endtime)
+            timeStamp = dateArray.strftime("%Y年%m月%d日-%H点%M分%S秒")
+            if not assignment.get(jsloop['plannable']['id']):
+                assignment[jsloop['plannable']['id']] = int(time.mktime(time.localtime()))
+                show_homework(jsloop, timeStamp, calender_activity, endtime, homeworktime, LastLoop)
+            util_time = int(time.mktime(time.localtime())) - int(assignment[jsloop['plannable']['id']])
+            if util_time >= int(remind):
+                # 只显示今日的
+
+                assignment[jsloop['plannable']['id']] = int(time.mktime(time.localtime()))
+                show_homework(jsloop, timeStamp, calender_activity, endtime, homeworktime, LastLoop)
+            else:
+                continue
+
+
+
+
 
 
 
